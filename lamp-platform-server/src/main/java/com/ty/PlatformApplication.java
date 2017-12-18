843be95dcc782f863b5c38b5d04a644c65aa46c8
tianyi <793187547@qq.com>
2017-12-18 16:20:36 +0800

    添加fastJson消息转换器 增加spring boot热更新 修改代码⌘+F9，重新build，速度比重新启动快

--- lamp-platform-server/src/main/java/com/ty/PlatformApplication.java
+++ lamp-platform-server/src/main/java/com/ty/PlatformApplication.java
@@ -1,9 +1,16 @@
 package com.ty;
 
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.util.List;
 
 /**
  * spring-boot 启动类
@@ -13,7 +20,27 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 @SpringBootApplication
 @ServletComponentScan
 @MapperScan("com.ty.*.mapper")
-public class PlatformApplication {
+public class PlatformApplication extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        super.configureMessageConverters(converters);
+        /*
+         * 1、需要先定义一个 convert 转换消息的对象;
+         * 2、添加fastJson 的配置信息，比如：是否要格式化返回的json数据;
+         * 3、在convert中添加配置信息.
+         * 4、将convert添加到converters当中.
+         */
+        // 1、需要先定义一个 convert 转换消息的对象;
+        FastJsonHttpMessageConverter fastConverter = new FastJsonHttpMessageConverter();
+        //2、添加fastJson 的配置信息，比如：是否要格式化返回的json数据;
+        FastJsonConfig fastJsonConfig = new FastJsonConfig();
+        fastJsonConfig.setSerializerFeatures(SerializerFeature.PrettyFormat);
+        //3、在convert中添加配置信息.
+        fastConverter.setFastJsonConfig(fastJsonConfig);
+        //4、将convert添加到converters当中.
+        converters.add(fastConverter);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(PlatformApplication.class, args);
