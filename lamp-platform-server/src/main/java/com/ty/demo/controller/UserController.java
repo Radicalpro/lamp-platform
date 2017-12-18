25586b9d2abe77e204457d03efcde8bdbafc8d5e
tianyi <793187547@qq.com>
2017-12-18 16:56:55 +0800

    sql日志打印

--- lamp-platform-server/src/main/java/com/ty/demo/controller/UserController.java
+++ lamp-platform-server/src/main/java/com/ty/demo/controller/UserController.java
@@ -2,6 +2,7 @@ package com.ty.demo.controller;
 
 import com.ty.demo.bean.User;
 import com.ty.demo.mapper.UserMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/user")
+@Slf4j
 public class UserController {
 
     @Autowired
@@ -18,6 +20,13 @@ public class UserController {
 
     @RequestMapping("/selectByPrimaryKey")
     public User selectByPrimaryKey() {
+///        User user = new User();
+//        user.setId(2);
+//        user.setUsername("ty");
+//        user.setPassword("123456");
+//        user.setRoleId(0);
+//        return user;
+        log.info("日志打印");
         return userMapper.selectByPrimaryKey(1);
     }
 }
